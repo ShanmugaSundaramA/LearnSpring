aa6df073e3e6efea19665c17b1ef85fec9ff2ff6
ShanmugaSundaram <sundarvikram08@gmail.com>
2023-04-09 09:00:27 +0100

    injecting value using @value annotation.

--- src/main/java/com/sundar/PropertyFile/Student.java
+++ src/main/java/com/sundar/PropertyFile/Student.java
@@ -1,17 +1,22 @@
 package com.sundar.PropertyFile;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student {
     
 	private int id;
     private String name;
     private String Std;
    
+    @Value("101")
 	public void setId(int id) {
 		this.id = id;
 	}
+    @Value("Sundaram")
 	public void setName(String name) {
 		this.name = name;
 	}
+    @Value("11th")
 	public void setStd(String std) {
 		Std = std;
 	}
