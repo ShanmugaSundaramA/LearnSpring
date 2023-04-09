5c1950931504313c1855c5960b0281c378792199
ShanmugaSundaram <sundarvikram08@gmail.com>
2023-04-09 05:59:36 +0100

    injecting object dependency using autowire="constructor" in bean tag

--- src/main/java/com/sundar/AutoWiring/Engine.java
+++ src/main/java/com/sundar/AutoWiring/Engine.java
@@ -1,8 +1,14 @@
 package com.sundar.AutoWiring;
 
 public class Engine {
+  
+	private String colour;
+	
+    public Engine(String colour) {
+		this.colour = colour;
+	}
 
-    public void engineCC() {
-    	System.out.println("engine Started..");
+	public void engineCC() {
+    	System.out.println(colour+" colour engine Started..");
     }
 }
