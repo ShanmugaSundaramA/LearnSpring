25529c5787820da6e97e84ac5f51b47834ccde15
ShanmugaSundaram <sundarvikram08@gmail.com>
2023-04-09 06:15:35 +0100

    using @autowired annotation.

--- src/main/java/com/sundar/AutoWiring/Bike.java
+++ src/main/java/com/sundar/AutoWiring/Bike.java
@@ -1,16 +1,20 @@
 package com.sundar.AutoWiring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Bike {
 	private Engine engine;
 
 	public Bike() {System.out.println("Default Construtor");}
 	
+	//@Autowired
 	public Bike(Engine engine) {
 		super();
 		this.engine = engine;
 		System.out.println("constructor method is called.");
 	}
-
+    
+	@Autowired
 	public void setEngine(Engine engine) {
 		this.engine = engine;
 		System.out.println("setter method is called.");
