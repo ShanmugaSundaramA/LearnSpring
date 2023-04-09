31f1848a84721e01d51fa3b1510c0ad7b1614e69
ShanmugaSundaram <sundarvikram08@gmail.com>
2023-04-09 05:46:45 +0100

    injecting object using constructor argu with ,ref attri.

--- src/main/java/com/sundar/AutoWiring/Bike.java
+++ src/main/java/com/sundar/AutoWiring/Bike.java
@@ -3,15 +3,17 @@ package com.sundar.AutoWiring;
 public class Bike {
 	private Engine engine;
 
-	public Bike() {}
+	public Bike() {System.out.println("Default Construtor");}
 	
 	public Bike(Engine engine) {
 		super();
 		this.engine = engine;
+		System.out.println("constructor method is called.");
 	}
 
 	public void setEngine(Engine engine) {
 		this.engine = engine;
+		System.out.println("setter method is called.");
 	}
 	
 	public void speed() {
